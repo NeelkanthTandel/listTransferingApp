e134315a06749442568e5f1fa45db5af377769dc
Neelkanth Tandel <tandelneelkanth@gmail.com>
2021-08-12 12:16:59 +0530

    updated App.js

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import MainNavigator from "./navigation/MainNavigator";
 
 export default function App() {
-   return <MainNavigator />;
+   return (
+      <View style={styles.container}>
+         <Text>Let's Start</Text>
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
