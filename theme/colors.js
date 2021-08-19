9104f0815773dfe8bfef4f6972385ebdb30d41ca
Neelkanth Tandel <tandelneelkanth@gmail.com>
2021-08-19 22:59:21 +0530

    added colors constant

---
+++ theme/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+   headerBgColor: "#1E2022",
+   headerTitle: "#E8F0F2",
+   backgroundColor: "#E8F0F2",
+   primary: "#A2DBFA",
+   secondary: "#495E74",
+   selected: "rgba(73, 94, 116, 0.5)",
+   textPrimary: "#1E2022",
+   textSecondary: "#495E74",
+};
+
+export default colors;
