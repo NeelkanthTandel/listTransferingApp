edb800425aa1cda7730dbb66a980bfe95d6bfa9b
Neelkanth Tandel <tandelneelkanth@gmail.com>
2021-09-09 15:17:22 +0530

    updated mainNavigator

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -179,7 +179,13 @@ const MainNavigator = () => {
                </Stack.Navigator>
             </NavigationContainer>
          ) : (
-            <View>
+            <View
+               style={{
+                  flex: 1,
+                  justifyContent: "center",
+                  alignItems: "center",
+               }}
+            >
                <Text>Loading...</Text>
             </View>
          )}
